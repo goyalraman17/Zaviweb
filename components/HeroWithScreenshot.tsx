7a00b94fe0e4acc045b158f0b222b00d4c49cef3
Raman Goyal <ramangoyal@Ramans-MacBook-Air.local>
2026-01-24 15:48:11 +0530

    Redesign hero section with visual process flow, feature ribbon, and floating app icons

--- components/HeroWithScreenshot.tsx
+++ components/HeroWithScreenshot.tsx
@@ -199,8 +199,9 @@ export default function HeroWithScreenshot() {
               transition={{ delay: 0.9, duration: 0.5, type: "spring", stiffness: 100 }}
             >
               {/* Floating App Icons around CTA */}
+              {/* Gmail */}
               <motion.div
-                className="absolute -left-12 top-0 w-10 h-10 bg-white rounded-xl shadow-lg flex items-center justify-center p-2 hidden lg:flex"
+                className="absolute -left-16 top-0 w-10 h-10 bg-white rounded-xl shadow-lg flex items-center justify-center p-2 hidden lg:flex"
                 animate={{ y: [0, -10, 0] }}
                 transition={{ duration: 3, repeat: Infinity, ease: "easeInOut" }}
               >
@@ -208,13 +209,70 @@ export default function HeroWithScreenshot() {
                   <path fill="currentColor" d="M24 5.457v13.909c0 .904-.732 1.636-1.636 1.636h-3.819V11.73L12 16.64l-6.545-4.91v9.273H1.636A1.636 1.636 0 0 1 0 19.366V5.457c0-2.023 2.309-3.178 3.927-1.964L12 9.545l8.073-6.052C21.69 2.28 24 3.434 24 5.457z" />
                 </svg>
               </motion.div>
+
+              {/* Slack */}
               <motion.div
-                className="absolute -right-12 bottom-0 w-10 h-10 bg-white rounded-xl shadow-lg flex items-center justify-center p-2 hidden lg:flex"
+                className="absolute -right-16 bottom-0 w-10 h-10 bg-white rounded-xl shadow-lg flex items-center justify-center p-2 hidden lg:flex"
                 animate={{ y: [0, 10, 0] }}
                 transition={{ duration: 3, repeat: Infinity, ease: "easeInOut", delay: 1 }}
               >
                 <svg viewBox="0 0 24 24" className="w-full h-full text-[#4A154B]">
                   <path fill="currentColor" d="M5.042 15.165a2.528 2.528 0 0 1-2.52 2.523A2.528 2.528 0 0 1 0 15.165a2.527 2.527 0 0 1 2.522-2.52h2.52v2.52zM6.313 15.165a2.527 2.527 0 0 1 2.521-2.52 2.527 2.527 0 0 1 2.521 2.52v6.313A2.528 2.528 0 0 1 8.834 24a2.528 2.528 0 0 1-2.521-2.522v-6.313zM8.834 5.042a2.528 2.528 0 0 1-2.521-2.52A2.528 2.528 0 0 1 8.834 0a2.528 2.528 0 0 1 2.521 2.522v2.52H8.834zM8.834 6.313a2.528 2.528 0 0 1 2.521 2.521 2.528 2.528 0 0 1-2.521 2.521H2.522A2.528 2.528 0 0 1 0 8.834a2.528 2.528 0 0 1 2.522-2.521h6.312zM18.956 8.834a2.528 2.528 0 0 1 2.522-2.521A2.528 2.528 0 0 1 24 8.834a2.528 2.528 0 0 1-2.522 2.521h-2.522V8.834zM17.688 8.834a2.528 2.528 0 0 1-2.523 2.521 2.527 2.527 0 0 1-2.52-2.521V2.522A2.527 2.527 0 0 1 15.165 0a2.528 2.528 0 0 1 2.523 2.522v6.312zM15.165 18.956a2.528 2.528 0 0 1 2.523 2.522A2.528 2.528 0 0 1 15.165 24a2.527 2.527 0 0 1-2.52-2.522v-2.522h2.52zM15.165 17.688a2.527 2.527 0 0 1-2.52-2.523 2.526 2.526 0 0 1 2.52-2.52h6.313A2.527 2.527 0 0 1 24 15.165a2.528 2.528 0 0 1-2.522 2.523h-6.313z" />
+                </svg>
+              </motion.div>
+
+              {/* WhatsApp */}
+              <motion.div
+                className="absolute -left-24 bottom-4 w-9 h-9 bg-white rounded-xl shadow-lg flex items-center justify-center p-1.5 hidden lg:flex"
+                animate={{ y: [0, 8, 0] }}
+                transition={{ duration: 4, repeat: Infinity, ease: "easeInOut", delay: 0.5 }}
+              >
+                <svg viewBox="0 0 24 24" className="w-full h-full text-[#25D366]">
+                  <path fill="currentColor" d="M17.472 14.382c-.297-.149-1.758-.867-2.03-.967-.273-.099-.471-.148-.67.15-.197.297-.767.966-.94 1.164-.173.199-.347.223-.644.075-.297-.15-1.255-.463-2.39-1.475-.883-.788-1.48-1.761-1.653-2.059-.173-.297-.018-.458.13-.606.134-.133.298-.347.446-.52.149-.174.198-.298.298-.497.099-.198.05-.371-.025-.52-.075-.149-.669-1.612-.916-2.207-.242-.579-.487-.5-.669-.51-.173-.008-.371-.01-.57-.01-.198 0-.52.074-.792.372-.272.297-1.04 1.016-1.04 2.479 0 1.462 1.065 2.875 1.213 3.074.149.198 2.096 3.2 5.077 4.487.709.306 1.262.489 1.694.625.712.227 1.36.195 1.871.118.571-.085 1.758-.719 2.006-1.413.248-.694.248-1.289.173-1.413-.074-.124-.272-.198-.57-.347m-5.421 7.403h-.004a9.87 9.87 0 01-5.031-1.378l-.361-.214-3.741.982.998-3.648-.235-.374a9.86 9.86 0 01-1.51-5.26c.001-5.45 4.436-9.884 9.888-9.884 2.64 0 5.122 1.03 6.988 2.898a9.825 9.825 0 012.893 6.994c-.003 5.45-4.437 9.884-9.885 9.884m8.413-18.297A11.815 11.815 0 0012.05 0C5.495 0 .16 5.335.157 11.892c0 2.096.547 4.142 1.588 5.945L.057 24l6.305-1.654a11.882 11.882 0 005.683 1.448h.005c6.554 0 11.89-5.335 11.893-11.893a11.821 11.821 0 00-3.48-8.413z" />
+                </svg>
+              </motion.div>
+
+              {/* ChatGPT */}
+              <motion.div
+                className="absolute -right-24 top-4 w-9 h-9 bg-white rounded-xl shadow-lg flex items-center justify-center p-1.5 hidden lg:flex"
+                animate={{ y: [0, -8, 0] }}
+                transition={{ duration: 4, repeat: Infinity, ease: "easeInOut", delay: 1.5 }}
+              >
+                <svg viewBox="0 0 24 24" className="w-full h-full text-[#10a37f]">
+                  <path fill="currentColor" d="M22.282 9.821a5.985 5.985 0 0 0-.516-4.91 6.046 6.046 0 0 0-6.51-2.9A6.065 6.065 0 0 0 4.981 4.18a5.985 5.985 0 0 0-3.998 2.9 6.046 6.046 0 0 0 .743 7.097 5.98 5.98 0 0 0 .51 4.911 6.051 6.051 0 0 0 6.515 2.9A5.985 5.985 0 0 0 13.26 24a6.056 6.056 0 0 0 5.772-4.206 5.99 5.99 0 0 0 3.997-2.9 6.056 6.056 0 0 0-.747-7.073zM13.26 22.43a4.476 4.476 0 0 1-2.876-1.04l.141-.081 4.779-2.758a.795.795 0 0 0 .392-.681v-6.737l2.02 1.168a.071.071 0 0 1 .038.052v5.583a4.504 4.504 0 0 1-4.494 4.494zM3.6 18.304a4.47 4.47 0 0 1-.535-3.014l.142.085 4.783 2.759a.771.771 0 0 0 .78 0l5.843-3.369v2.332a.08.08 0 0 1-.033.062L9.74 19.95a4.5 4.5 0 0 1-6.14-1.646zM2.34 7.896a4.485 4.485 0 0 1 2.366-1.973V11.6a.766.766 0 0 0 .388.676l5.815 3.355-2.02 1.168a.076.076 0 0 1-.071 0l-4.83-2.786A4.504 4.504 0 0 1 2.34 7.872zm16.597 3.855l-5.833-3.387L15.119 7.2a.076.076 0 0 1 .071 0l4.83 2.791a4.494 4.494 0 0 1-.676 8.105v-5.678a.79.79 0 0 0-.407-.667zm2.01-3.023l-.141-.085-4.774-2.782a.776.776 0 0 0-.785 0L9.409 9.23V6.897a.066.066 0 0 1 .028-.061l4.83-2.787a4.5 4.5 0 0 1 6.68 4.66zm-12.64 4.135l-2.02-1.164a.08.08 0 0 1-.038-.057V6.075a4.5 4.5 0 0 1 7.375-3.453l-.142.08L8.704 5.46a.795.795 0 0 0-.393.681zm1.097-2.365l2.602-1.5 2.607 1.5v2.999l-2.597 1.5-2.607-1.5z" />
+                </svg>
+              </motion.div>
+
+              {/* Reddit */}
+              <motion.div
+                className="absolute -left-6 -top-12 w-8 h-8 bg-white rounded-xl shadow-lg flex items-center justify-center p-1.5 hidden lg:flex"
+                animate={{ x: [0, 5, 0], y: [0, -5, 0] }}
+                transition={{ duration: 5, repeat: Infinity, ease: "easeInOut", delay: 2 }}
+              >
+                <svg viewBox="0 0 24 24" className="w-full h-full text-[#FF4500]">
+                  <path fill="currentColor" d="M24 11.5c0-1.65-.67-3.14-1.74-4.23-.01 0-.01-.01-.02-.01l-.22-.22c-1.33-1.33-3.48-1.33-4.81 0l-1.19 1.19c-1.42-.45-3-.72-4.7-.78l.85-3.95 2.73.58c.07.82.76 1.47 1.6 1.47 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .12.01.24.03.36l-3.34-.71c-.13-.03-.26 0-.36.08s-.16.21-.18.34L7.88 7.51C6.18 7.57 4.6 7.84 3.18 8.29l-1.19-1.19c-1.33-1.33-3.48-1.33-4.81 0l-.22.22c-1.07 1.09-1.74 2.58-1.74 4.23 0 1.56.59 2.97 1.56 4.04l-.06.46C-3.3 20.04-1.3 24 12 24s15.3-3.96 15.3-7.95l-.06-.46c.97-1.07 1.56-2.48 1.56-4.04z" />
+                </svg>
+              </motion.div>
+
+              {/* Messenger */}
+              <motion.div
+                className="absolute -right-6 -bottom-12 w-8 h-8 bg-white rounded-xl shadow-lg flex items-center justify-center p-1.5 hidden lg:flex"
+                animate={{ x: [0, -5, 0], y: [0, 5, 0] }}
+                transition={{ duration: 5, repeat: Infinity, ease: "easeInOut", delay: 2.5 }}
+              >
+                <svg viewBox="0 0 24 24" className="w-full h-full text-[#0084FF]">
+                  <path fill="currentColor" d="M12 0C5.373 0 0 4.974 0 11.111c0 3.498 1.744 6.614 4.469 8.654V24l4.088-2.242c1.092.303 2.246.464 3.443.464 6.627 0 12-4.974 12-11.111C24 4.974 18.627 0 12 0zm1.291 14.194l-3.21-3.43-6.27 3.43 6.898-7.331 3.281 3.431 6.199-3.431-6.898 7.331z" />
+                </svg>
+              </motion.div>
+
+              {/* WeChat */}
+              <motion.div
+                className="absolute left-1/2 -bottom-16 -translate-x-1/2 w-8 h-8 bg-white rounded-xl shadow-lg flex items-center justify-center p-1.5 hidden lg:flex"
+                animate={{ scale: [1, 1.1, 1] }}
+                transition={{ duration: 6, repeat: Infinity, ease: "easeInOut", delay: 3 }}
+              >
+                <svg viewBox="0 0 24 24" className="w-full h-full text-[#07C160]">
+                  <path fill="currentColor" d="M8.28 1.4c-4.42 0-8 3.08-8 6.88 0 2.1 1.1 3.96 2.83 5.25l-.7 2.12 2.45-1.22c.45.1 1 .15 1.42.15 4.42 0 8-3.08 8-6.88S12.7 1.4 8.28 1.4zm6.82 7.7c-3.6 0-6.52 2.48-6.52 5.54s2.92 5.54 6.52 5.54c.36 0 .73-.04 1.1-.12l1.96 1L17.6 19.3c1.45-1 2.36-2.52 2.36-4.2 0-3.05-2.92-5.54-6.52-5.54z" />
                 </svg>
               </motion.div>
 
